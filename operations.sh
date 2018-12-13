f721219693f8cde8aa99e674b9444bf656b9718e
Tiger Sachse <tgsachse@gmail.com>
2018-12-13 00:18:53 -0500

    averaging bug fix

--- operations.sh
+++ operations.sh
@@ -1,16 +1,24 @@
-BUILD_DIR="/tmp/temporary_build_dir_parallel_divsufsort"
+# Operations script to make working with this repository easier.
+# Written by Tiger Sachse.
 
+BUILD_DIR="/tmp/temporary_build_dir_parallel_divsufsort"
+PROGRAM_NAME="chronograph"
+
+# Install necessary dependencies for this repository to function.
 install_dependencies() {
     if [[ $EUID -ne 0 ]]; then
         echo "This operation must be run as root." 
         exit 1
     fi
 
+    # This function needs these packages to build the required divsufsort library.
     apt install git cmake make g++-7
 
+    # Override any existing compilers and use the g++-7 compiler.
     export CC="g++-7;$CC"
     export CXX="g++-7;$CXX"
 
+    # Clone the divsufsort library repository into a temporary build directory.
     rm -r $BUILD_DIR
     git clone https://github.com/jlabeit/parallel-divsufsort.git $BUILD_DIR
     cd $BUILD_DIR
@@ -18,18 +26,21 @@ install_dependencies() {
     mkdir build
     cd build
 
+    # Install the divsufsort library using cmake.
     cmake ..
     make
     make install
 }
 
+# Run the repository driver to test the divsufsort library.
 run() {
-    g++-7 -fcilkplus source/chronograph.cpp -o chronograph \
+    g++-7 -fcilkplus source/$PROGRAM_NAME.cpp -o $PROGRAM_NAME \
         -l cilkrts -l divsufsort -l libprange &&
-    ./chronograph "$@"
-    rm -rf chronograph
+    ./$PROGRAM_NAME "$@"
+    rm -rf $PROGRAM_NAME
 }
 
+# Main entry point of this script.
 case "$1" in
     "--install-deps")
         install_dependencies
--- source/chronograph.cpp
+++ source/chronograph.cpp
@@ -1,24 +1,47 @@
+// Driver program to test the parallel-divsufsort library.
+// Written by Tiger Sachse.
+
+#include <cmath>
 #include <string>
 #include <chrono>
 #include <fstream>
 #include <iostream>
 #include <divsufsort.h>
+#include <cilk/cilk_api.h>
 
 // Run timed divsufsort tests for a string using a range of thread counts.
 void run_tests(const std::string& text, int max_threads, int max_runs) {
-    int64_t size = text.size();
-    int64_t *suffix_array = new int64_t[size];
+    auto size = text.size();
+    auto suffix_array = new int64_t[size];
+    auto total_times = new std::chrono::milliseconds[max_threads]();
 
+    // For each run, test the function using a range of thread caps.
     for (int run = 0; run < max_runs; run++) {
-        auto start = std::chrono::steady_clock::now();
-        divsufsort((sauchar_t*) text.data(), suffix_array, size);
-        auto end = std::chrono::steady_clock::now();
-        auto diff = end - start;
+        for (int thread = 1; thread <= max_threads; thread++) {
+            char* buffer = new char[(int) std::log10(max_threads) + 2]();
+            sprintf(buffer, "%d", thread);
+            __cilkrts_set_param("nworkers", buffer);
+            delete[] buffer;
 
-        auto seconds = std::chrono::duration <double, std::milli> (diff).count() / 1000.0;
-        std::cout << seconds << "\n";
+            auto start = std::chrono::steady_clock::now();
+            divsufsort((sauchar_t*) text.data(), suffix_array, size);
+            auto end = std::chrono::steady_clock::now();
+
+            // Save the time difference in the total_times array. The
+            // average across all runs will be calculated from this sum later.
+            auto difference = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+            total_times[thread - 1] += difference;
+        }
     }
 
+    // Print the average run time for each thread cap.
+    for (int thread = 1; thread <= max_threads; thread++) {
+        printf("%d thread%s: ", thread, (thread == 1) ? "" : "s");
+        printf("%lf seconds\n", total_times[thread - 1].count() / 1000.0 / max_runs);
+    }
+
+    // Clean up after yourself!
+    delete[] total_times;
     delete[] suffix_array;
 }
 
@@ -26,22 +49,23 @@ void run_tests(const std::string& text, int max_threads, int max_runs) {
 int main(int argument_count, char* arguments[]) {
 
     // If the user has not entered enough arguments, scream about it.
-    if (argument_count < 3) {
-        std::cout << "Usage: ./chronograph [max threads] [file]\n";
+    if (argument_count < 4) {
+        std::cout << "Usage: ./chronograph [file] [max threads] [max runs]\n";
 
         return -1;
     }
-
-    // Parse the max_threads argument from the input vector.
-    int max_threads = std::stoi(arguments[1], NULL, 10);
 
     // Attempt to open the text file.
-    std::ifstream input(arguments[2]);
+    std::ifstream input(arguments[1]);
     if (input.fail()) {
-        std::cout << "File '" << arguments[2] << "' could not be read.\n";
+        std::cout << "File '" << arguments[1] << "' could not be read.\n";
 
         return -1;
     }
+
+    // Parse max_threads and max_runs from the input vector.
+    int max_threads = std::stoi(arguments[2], NULL, 10);
+    int max_runs = std::stoi(arguments[3], NULL, 10);
 
     // Create a string from the text file.
     std::string text;
@@ -50,7 +74,7 @@ int main(int argument_count, char* arguments[]) {
     input.seekg(0, std::ios::beg);
     text.assign((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
 
-    run_tests(text, max_threads, 10);
+    run_tests(text, max_threads, max_runs);
 
     return 0;
 }
