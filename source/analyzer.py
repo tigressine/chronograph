3d3f0e9bee54baaab229a855b2f6fb1602e8a325
Tiger Sachse <tiger@sachse.io>
2018-12-13 18:14:15 -0500

    graphs are cleaner, added efficiency graph

--- source/analyzer.py
+++ source/analyzer.py
@@ -1,22 +1,32 @@
+"""Analyze parallelized divsufsort data.
+
+Written by Tiger Sachse.
+"""
 import re
 import sys
-import matplotlib.pyplot as pyplot
 from pathlib import Path
+import matplotlib.pyplot as pyplot
 
 LINE_FORMAT = (
     r'^Thread count: (?P<count>[0-9]+) \| '
     + r'Seconds: (?P<seconds>[0-9]+(\.[0-9]+)?)\n$'
 )
 
+def percent_change(num1, num2):
+    return (num1 - num2) / abs(num1) * 100
+
 try:
     counts = []
     seconds = []
+
+    # Open the input data file.
     with open(sys.argv[1], 'r') as file_pointer:
         
         # Skip the header line.
         file_pointer.readline()
 
-        # For each line in the file, extract the count and seconds.
+        # For each line in the file, extract the count and seconds, then
+        # add each to its respective list.
         for line in file_pointer:
             line_match = re.match(LINE_FORMAT, line)
             if line_match:
@@ -34,8 +44,21 @@ except FileNotFoundError:
     print('The data file provided could not be found/opened.')
     exit(0)
 
+# Plot the counts and seconds using matplotlib.
 pyplot.plot(counts, seconds)
 pyplot.xlabel('Thread count')
 pyplot.ylabel('Seconds')
 
+# Save the graph as a PNG.
 pyplot.savefig(str(Path(sys.argv[1]).stem) + '_seconds_graph.png')
+pyplot.clf()
+
+changes = []
+for position, amount in enumerate(seconds[:-1]):
+    changes.append(percent_change(amount, seconds[position + 1]))
+
+pyplot.plot(counts[1:], changes)
+pyplot.xlabel('Thread count')
+pyplot.ylabel('Percent change in execution time')
+
+pyplot.savefig(str(Path(sys.argv[1]).stem) + '_changes_graph.png')
